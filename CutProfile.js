c7d67ecc2268ca7e5ff1c7cac9f0b026c5514b32
rytokd <108518341+rytokd@users.noreply.github.com>
2024-09-21 01:39:25 +0900

    Update CutProfile.js

--- CutProfile.js
+++ CutProfile.js
@@ -3,24 +3,24 @@ javascript: (function(d) {
     const getById = (id) => d.getElementById(id);
     alert('さんこんにちわ');
 
-    // fetch('https://rytokd.github.io/Bookmarklet/messageList.json')
-    //     .then((response) => {
-    //         if (!response.ok) {
-    //             throw new Error('Network response was not ok');
-    //         }
-    //         return response.json();
-    //     })
-    //     .then((msgList) => {
-    //         if (msgList && msgList.first !== undefined) {
-    //             getById("message_form_body").value = select('.topic-header_contents_name.text-ellipsis').textContent + msgList.first;
-    //             select(".topic-footer").style.height = "250px";
-    //             getById("message_form_body").style.height = "250px";
-    //             select('[type="submit"]').classList.add("topic-footer_message-form_submit", "is-active");
-    //         } else {
-    //             console.error('msgList.first is undefined');
-    //         }
-    //     })
-    //     .catch((error) => {
-    //         console.error('Fetch error:', error);
-    //     });
+    fetch('https://rytokd.github.io/Bookmarklet/messageList.json')
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then((msgList) => {
+            if (msgList && msgList.first !== undefined) {
+                getById("message_form_body").value = select('.topic-header_contents_name.text-ellipsis').textContent + msgList.first;
+                select(".topic-footer").style.height = "250px";
+                getById("message_form_body").style.height = "250px";
+                select('[type="submit"]').classList.add("topic-footer_message-form_submit", "is-active");
+            } else {
+                console.error('msgList.first is undefined');
+            }
+        })
+        .catch((error) => {
+            console.error('Fetch error:', error);
+        });
 })(document);
