3450e2a3a2c7d79c99039da654cbcb4f8a6f01ea
rytokd <108518341+rytokd@users.noreply.github.com>
2024-09-21 02:13:58 +0900

    Create holiday.js

---
+++ holiday.js
@@ -0,0 +1,27 @@
+javascript: (function(d) {
+    const select = (selector) => d.querySelector(selector);
+    const getById = (id) => d.getElementById(id);
+
+    fetch('https://rytokd.github.io/Bookmarklet/messageList.json')
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then((msgList) => {
+            if (msgList && msgList.first !== undefined) {
+                getById("message_form_body").value = select('.topic-header_contents_name.text-ellipsis').textContent + msgList.holiday;
+                select(".topic-footer").style.height = "250px";
+                getById("message_form_body").style.height = "250px";
+                select('[type="submit"]').classList.add("topic-footer_message-form_submit", "is-active");
+                select('[type="submit"]').classList.remove("is-disabled");
+                select('[type="submit"]').disabled = false;
+            } else {
+                console.error('msgList.first is undefined');
+            }
+        })
+        .catch((error) => {
+            console.error('Fetch error:', error);
+        });
+})(document);
