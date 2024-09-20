970ee98c0c56303d996f91d28f15759b966f014b
rytokd <108518341+rytokd@users.noreply.github.com>
2024-09-21 00:41:49 +0900

    Update CutProfile.js

--- CutProfile.js
+++ CutProfile.js
@@ -1,1 +1,14 @@
-
+javascript: (function(d) {
+    const select = d.querySelector.bind(d);
+    const getById = d.getElementById.bind(d)
+    fetch('./msgList.json')
+    .then((response)=>{return response.json()})
+    .then( (msgList)=>{
+        console.log(msgList);
+        alert(msgList.first);
+    });
+    getById("message_form_body").value = select('.topic-header_contents_name.text-ellipsis').innerHTML + msgList.first;
+    select(".topic-footer").style.height = "250px";
+    getById("message_form_body").style.height = "250px";
+    select('[type="submit"]').classList.add("topic-footer_message-form_submit","is-active");
+})(document);
