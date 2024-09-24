68677960b95f9ad520878e6f2b4df04eeff4709e
rytokd <108518341+rytokd@users.noreply.github.com>
2024-09-24 19:51:11 +0900

    Update autopage.js

--- autopage.js
+++ autopage.js
@@ -1,27 +1,51 @@
-javascript: (function(d) {
-    const select = (selector) => d.querySelector(selector);
-    const getById = (id) => d.getElementById(id);
+javascript: (function() {
+    // AutoPagerizeの設定
+    var AutoPagerize = {
+        nextPageSelector: '.paging_next a', // 次のページへのリンクセレクタ
+        contentSelector: '.sp-container', // コンテンツを含むセレクタ
+        currentPageInfoSelector: '[data-paging-info]', // 現在のページ情報を持っているセレクタ
+        loadMore: function() {
+            var nextPageLink = document.querySelector(this.nextPageSelector);
+            if (nextPageLink) {
+                var pageNum = parseInt(nextPageLink.href.match(/page=(\d+)/)[1]);
+                var container = document.querySelector(this.contentSelector);
+                fetch(nextPageLink.href)
+                    .then(response => response.text())
+                    .then(html => {
+                        var parser = new DOMParser();
+                        var doc = parser.parseFromString(html, 'text/html');
+                        var newContent = doc.querySelector(this.contentSelector).innerHTML;
+                        container.insertAdjacentHTML('beforeend', newContent);
+                        const e = document.getElementsByClassName('header_banner');
+                        while (e.length) {
+                            e.item(0).remove()
+                        };
+                        const f = document.getElementsByClassName('head-missing-topics-list');
+                        while (f.length) {
+                            f.item(0).remove()
+                        };
+                        
+                        // 更新されたページ数を適切に設定
+                        // 以下の行は必要に応じて調整
+                        nextPageLink.href = nextPageLink.href.replace(/page=(\d+)/, 'page=' + (pageNum + 1));
+                    })
+                    .catch(err => console.error('Failed to load page: ', err));
+            }
+        },
+        init: function() {
+            var currentInfo = document.querySelector(this.currentPageInfoSelector);
+            if (currentInfo) {
+                var observer = new IntersectionObserver(entries => {
+                    if (entries[0].isIntersecting) {
+                        this.loadMore();
+                    }
+                });
 
-    fetch('https://rytokd.github.io/Bookmarklet/messageList.json')
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
+                observer.observe(currentInfo);
             }
-            return response.json();
-        })
-        .then((msgList) => {
-            if (msgList && msgList.first !== undefined) {
-                getById("message_form_body").value = select('.topic-header_contents_name.text-ellipsis').textContent + msgList.first;
-                select(".topic-footer").style.height = "250px";
-                getById("message_form_body").style.height = "250px";
-                select('[type="submit"]').classList.add("topic-footer_message-form_submit", "is-active");
-                select('[type="submit"]').classList.remove("is-disabled");
-                select('[type="submit"]').disabled = false;
-            } else {
-                console.error('msgList.first is undefined');
-            }
-        })
-        .catch((error) => {
-            console.error('Fetch error:', error);
-        });
-})(document);
+        }
+    };
+
+    // AutoPagerizeを初期化
+    AutoPagerize.init();
+})();
