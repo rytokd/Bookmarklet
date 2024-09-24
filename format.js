f056554ec1891d68ac6dce7597671db8c1c19098
rytokd <108518341+rytokd@users.noreply.github.com>
2024-09-25 01:57:04 +0900

    Update format.js

--- format.js
+++ format.js
@@ -1,5 +1,6 @@
 javascript: (function() {
 	document.querySelector(".message-read-receipt-appeal").remove();
+	document.querySelector(".secret-questions_link-space").remove();
 	const partnerMsg = document.querySelectorAll('.message_balloon');
 	partnerMsg.forEach(element => {
 		element.style.width = "1200px";
