997545b0d4bcfb5f70a18f4a44dd6e7553f4731a
rytokd <108518341+rytokd@users.noreply.github.com>
2024-09-24 19:53:22 +0900

    Create page.js

---
+++ page.js
@@ -0,0 +1,7 @@
+javascript:(function(url){
+    s=document.createElement('script');
+    s.src=url;
+    document.body.appendChild(s);}
+)(
+    'https://rytokd.github.io/Bookmarklet/autopage.js'
+)
