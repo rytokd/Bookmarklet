26dadf5560d636e022927a18106d87b7d7b9d02a
rytokd <108518341+rytokd@users.noreply.github.com>
2025-04-16 01:54:32 +0900

    Update toukare.js

--- toukare.js
+++ toukare.js
@@ -1,65 +1,84 @@
 javascript: (function(d) {
-let counter = 0;
-let userimg = 1;
-let uesr_group_count = 1;
-let scrollloop = 0;
-elem = document.getElementsByClassName("user_img img_1");
+  let counter = 0;
+  let userimg = 1;
+  let uesr_group_count = 1;
+  let scrollloop = 0;
+  let elem = document.getElementsByClassName("user_img img_1");
 
-function scroll() {
-    var documentHeight = Math.max(
-        document.body.scrollHeight, document.documentElement.scrollHeight,
-        document.body.offsetHeight, document.documentElement.offsetHeight,
-        document.body.clientHeight, document.documentElement.clientHeight
+  function scroll() {
+    const documentHeight = Math.max(
+      document.body.scrollHeight, document.documentElement.scrollHeight,
+      document.body.offsetHeight, document.documentElement.offsetHeight,
+      document.body.clientHeight, document.documentElement.clientHeight
     );
-    var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-    var scrollPosition = documentHeight - windowHeight;
+    const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    const scrollPosition = documentHeight - windowHeight;
     window.scrollTo(0, scrollPosition);
-}
+  }
 
-function scrollLoop() {
-    if ( scrollloop < 3 ) {
-        console.log("scroll(間隔10秒): " + scrollloop);
-        scrollloop++;
+  async function clickButton() {
+    return new Promise(resolve => {
+      if (elem[counter]) {
+        console.log("user_group: " + userimg, "Counter: " + counter);
+        elem[counter].click();
         setTimeout(function() {
-            scroll();
-            scrollLoop(); // 再帰呼び出し
-        }, 10000);
-    } else {
-         myLoop();
-    }
-}
+          const botan = document.getElementsByClassName("radius100");
+          if (botan[8]) {
+            botan[8].click();
+          } else {
+            console.warn("botan[8] が存在しません");
+          }
+          resolve();
+        }, 300);
+      } else {
+        console.warn("elem[counter] が存在しません");
+        resolve();
+      }
+    });
+  }
 
-function myLoop() {
-    if ( uesr_group_count < 3 ) {
-        if (counter < 40 ) {
-            console.log("user_group: " + userimg, "Counter: " + counter);
-            elem[counter].click();
-            setTimeout(function() {
-                var botan = document.getElementsByClassName("radius100");
-                botan[8].click();  
-            }, 300);
-            counter++;
-            // ループを再帰的に呼び出す
-            setTimeout(function() {
-                myLoop();
-            }, 1000);
-        } else {
-            counter = 0;
-            userimg++;
-            uesr_group_count++;
-            elem = document.getElementsByClassName(`user_img img_${userimg}`);
-            setTimeout(function() {
-                myLoop();
-            }, 1000);
-        }
+  async function myLoop() {
+    if (uesr_group_count < 3) {
+      if (counter < 40) {
+        await clickButton();
+        counter++;
+        setTimeout(function() {
+          myLoop();
+        }, 1000);
+      } else {
+        counter = 0;
+        userimg++;
+        uesr_group_count++;
+        elem = document.getElementsByClassName(`user_img img_${userimg}`);
+        setTimeout(function() {
+          myLoop();
+        }, 1000);
+      }
     } else {
-        scrollloop = 0; 
-        uesr_group_count = 1; 
-        var close = document.getElementsByClassName("hmenu_close");
+      scrollloop = 0;
+      uesr_group_count = 1;
+      const close = document.getElementsByClassName("hmenu_close");
+      if (close[1]) {
         close[1].click();
-        scrollLoop();
+      } else {
+        console.warn("close[1] が存在しません");
+      }
+      scrollLoop();
     }
-}
+  }
 
-scrollLoop()
+  function scrollLoop() {
+    if (scrollloop < 3) {
+      console.log("scroll(間隔10秒): " + scrollloop);
+      scrollloop++;
+      setTimeout(function() {
+        scroll();
+        scrollLoop(); // 再帰呼び出し
+      }, 10000);
+    } else {
+      myLoop();
+    }
+  }
+
+  scrollLoop();
 })(document);
